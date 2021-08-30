347eaf5eb91399c7073f90afa0f545ace4bc7b3f
fobnetmarketer <fobnetmarketer@gmail.com>
2021-08-30 23:31:29 +1200

    First Dev Merge
    First Dev Merge
    Andrew - nav
    Fred - main

--- components/navigation/navigation.jsx
+++ components/navigation/navigation.jsx
@@ -1,8 +1,5 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import Nav from "react-bootstrap/Nav";
-import {Navbar} from "react-bootstrap";
-import {NavDropdown} from "react-bootstrap";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import style from './navigation.module.css';
@@ -14,66 +11,175 @@ import style from './navigation.module.css';
 // if have to rebuild API in strapi just use staic tiltes to get a render
 
 export default function Navigation (props){
-
-  // console.log(props.navigation[0].slug)
-// console.log(props.navigation)
+// used to grab from strapi API
+  //  console.log(props.navigation[9].slug)
+  console.log(props.navigation[0].title)
+console.log(props.navigation)
 const [navBar, setNavBar] = useState(false);
-
-
 // open close function.
 const shownavBar = () => setNavBar(!navBar);
+// overlay nav open close state and function toggles state
+const [navOverlay, setnavOverlay] = useState(false);
+const shownavOverlay = () => setnavOverlay(!navOverlay);
+//
+// const [megaNav, setmegaNav] = useState(false);
+// const showmegaNav = () => setmegaNav(!megaNav);
+
   
 
-    return(
+  return(
     <>
-    {/* <h1 className={Styles.title}>Hello</h1> */}
+  {/* <OverlayNav showOverlay={shownavBar}/> */}
+  {/* className={ navOverlay ? `${style.overlayActive}` : `${style.overlay} */}
+  {/* About overlayNAv //////////////////  */}
+  <div className={style.alertStripe}>
+ <p className={style.alertTitle}>
+   ALERT
+ </p>
+ 
+</div>
+  <div className={ navOverlay ? `${style.overlayActive}` : `${style.overlay}`}>
+  <a  className={style.closebtn} 
+  onClick={shownavOverlay}>&times;</a>
+  <div  onClick={shownavOverlay} className={style.overlayContent}>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>
+</div>
+{/* ////////////////////////////////////////// */}
 <nav className={style.navbarContainer}>
-  <div className={style.logocontainer}>
-    <a target="/index" href="">
+<div className={style.logocontainer}>
       <img src="https://via.placeholder.com/80/#808080
 C/O https://placeholder.com//808080 ?Text=Digital.com
 C/O https://placeholder.com/" alt="place holder" />
-    </a>
   </div>
+ 
   <div className={style.navContainer}>
-  <div className={style.navBarItems}>
-{props.navigation.map(item =>(
-  <li className={style.links} key={item.id}>
-    <Link  className={style.slug}href={item.slug}>
-      {item.title}
-    </Link>
-    
-  </li>
 
-  ))}
+<div className={style.navBarItems}>
+
+<li className={style.links} 
+key={props.navigation[0].id}>
+   <div className={style.aboutButton} 
+onClick={shownavOverlay}>
+  {props.navigation[0].title}
+</div>
+</li>
+    <li className={style.links}>
+        <Link  
+        
+        href={props.navigation[1].slug}
+        >
+        {props.navigation[1].title}
+        </Link> 
+    </li>
+<li className={style.links}>
+<Link  
+
+href={props.navigation[2].slug}
+>
+{props.navigation[2].title}
+ </Link> 
+</li>
+
+<li className={style.dropbtn}>
+  {/* Mega-nav  layout  */}
+  <div className="">
+      {props.navigation[3].title}
+    <i className="fa fa-caret-down"></i>
+    <div className="">
+    
+    <div className={style.dropdownContent}>
+      <div className={style.header}>
+     
+      </div>
+      <div className={style.columnContainer}>
+      <div className={style.row}>
+       <div className={style.column}>
+          <h3 className={style.header}>Vaccinations & COVID-19 Info</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div className={style.column}>
+          <h3  className={style.header}>Resources</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div className={style.column}>
+          <h3 className={style.header}>Talanoa Videos</h3>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <div className={style.columnLast}>
+          <div className={style.imgContainer}>
+        
+          </div>
+        </div>
+      </div>
+      </div>
+     
+    </div>
+  </div>
+    </div>
+  
+    
+
+</li>
+
+    
+  
+    
+ 
 
 </div>
 {/* resposive nav button in nav */}
 <button className={style.icon} 
-    onClick={shownavBar}>
-      <i className="fa fa-bars"></i>
-    Button
+onClick={shownavBar}>
+  <i className="fa fa-bars"></i>
+  Button
 </button>
 
 
   </div> 
 </nav>
+
+
 <div className={ navBar ? `${style.responisveNavActive}` : `${style.responsiveNav}`} >
-  <div className={style.responsiveNavContainer}>
-        {props.navigation.map(item =>(
-          <li className={style.responsiveLinks} key={item.id}>
-            <Link href={item.slug}>
-                {item.title}
-            </Link>
-          </li>)
-        )}
-  </div>
+<div className={style.responsiveNavContainer}>
+{props.navigation.map(item =>(
+  <li className={style.responsiveLinks} key={item.id}>
+    <Link href={item.slug}>
+
+     
+      {item.title}
+    
+    </Link>
+    
+  </li>
+
+  ))}
+</div>
 </div>
 </>
 )
 
 
 }
+
+{/* <Link  
+onClick={shownavBar}
+className={style.slug}
+// href={props.navigation[9].slug}
+>
+  placeholder
+  {/* {props.navigation[9].title} */}
+// </Link> */}
+
 
 // {props.navigation.map(item=>(
 //   <li key={props.navigation.id}>
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,33 +4,96 @@ import Card from "../components/storycard"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import fetch from 'isomorphic-unfetch'
-import Styles from '../styles/home.module.css';
-import Utils from '../styles/utils.module.css';
-
 // style={{display:'flex', justifyContent:'center', margin:'20px'}}
 const Home = (props) => {
 
   //  console.log(props.movies)
   return (
+    // <h2> This will be the home page. <div className=""></div></h2>
+    //     <Container fluid>
+    //       <Row style={{justifyContent:'center'}}>
+    //       {props.movies.map(movie=>(
+    //         <div style={{margin:'2rem'}} >
+    //             <Card
+    //             key={movie.id}
+    //             movie={movie}
+    //             />
+    //         </div>
+    //       ))}
+    //       </Row>
 
-
+    //  </Container>
+   
+<>
 <section className={Styles.mainWrapper}>
-  <div className={Styles.sideBarLeft__Image}><h2>Side Bar Left</h2>
-  </div>
-  <div className={Styles.heroHome__Image}></div>
-  <div className={Styles.contentMain}><h2>Content</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <div className={Styles.sideBarRight__Image}><h2>Side Bar Right</h2>
-    <p class="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    
-  </div>
+      <div className={Styles.hero__Image}>
+      </div>
+      <div className={Styles.hero__Overlay}></div>
+      <div className={Styles.sideBarLeft__Image}><h2>Side Bar Left</h2></div>
+      <div className={Styles.contentMain}><h2>Content</h2>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ </div>
+      <div className={Styles.sideBarRight__Image}><h2>Side Bar Right</h2></div>
 </section>
 
+<section className={Styles.mainWrapper}>
+      <div className={Styles.hero__Image}>
+      </div>
+      <div className={Styles.hero__Overlay}></div>
+      <div className={Styles.sideBarLeft__Image}><h2>Side Bar Left</h2></div>
+      <div className={Styles.contentMain}><h2>Content</h2>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ </div>
+      <div className={Styles.sideBarRight__Image}><h2>Side Bar Right</h2></div>
+</section>
+
+<section className={Styles.mainWrapper}>
+      <div className={Styles.hero__Image}>
+      </div>
+      <div className={Styles.hero__Overlay}></div>
+      <div className={Styles.sideBarLeft__Image}><h2>Side Bar Left</h2></div>
+      <div className={Styles.contentMain}><h2>Content</h2>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+ </div>
+      <div className={Styles.sideBarRight__Image}><h2>Side Bar Right</h2></div>
+</section>
+
+<section>
+<div className={Styles.footer}><h2>Footer</h2></div>
+</section>
+</>
+
+
   )
+
+
 }
+
+
+
+// export async function getStaticProps(){
+//   // using dotenv and getting the url from env file.
+//   const {API_URL} = process.env
+
+//   // const res = await fetch(`${API_URL}/movies`)
+//   // const response = await res.json()
+//   const response = await axios.get(`${API_URL}/movies`)
+
+
+
+//   return {
+//     props: {
+//         movies: response.data
+//     }
+// }
+
+// }
 
 
 
