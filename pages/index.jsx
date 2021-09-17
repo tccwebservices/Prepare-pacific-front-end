5db3465108343d8c3636720ce60cf185613318b2
unknown <you@example.com>
2021-09-17 15:12:28 +1200

    added fullapge js

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,9 @@
 import Image from 'next/image'
 import axios from "axios";
 import fetch from 'isomorphic-unfetch'
+// IMoprt fullpahe js scroll
+import ReactFullpage from '@fullpage/react-fullpage'; // will return static version on server and "live" version on client
+
 
 // import Components
 import Footer from "../components/footer/footer"
@@ -33,64 +36,69 @@ import style from "../styles/Home.module.css"
 const Home = (props) => {
   return (
     <>
-    
-    {/* Overall Section Container */}
-    <div className={style.sectionsContainer}>
-    
-      {/* Section One (.sectionOne) */}
-      <section id="sectionOne" className={`${style.sectionWrapper} ${style.sectionOne}`}>
-    
-        {/* Grid Container */}
-        <div className={style.gridContainer}>
+     <ReactFullpage
+        // does the dot navigation
+          navigation
+          // for horizontal navigation and method above.
+          // pluginWrapper={pluginWrapper}
+          // onLeave={this.onLeave.bind(this)}
+          // scrollHorizontally = {true}
+
+          render={comp =>
+          <ReactFullpage.Wrapper>
+            {/* Section1 ////////// USing className section for ReacFullpage.Wrapper  */}
+            <div className="section">
+              <div className={`${style.sectionWrapper} ${style.sectionOne}`}>
+                {/* Grid Container */}
+                <div className={style.gridContainer}>
           
-          {/* Grid Section Column 1 */}
-          <div className={style.gridSections}>
-            <div className={style.flexContainer}>
-              <div className={style.pacificPattern}>
-              {/* <PatternBrown /> */}
-              </div>
-            </div>
-          </div>
-          
-          {/* Grid Section Column 2 */}
-          <div className={style.gridSections}>
-              
-              {/* Grid Section Container */}
-              <div className={style.gridTwoColumnContainer}>
-                <div className={style.gridColumn}>
-                  <div className={style.flexContainer}>
-                    <div className={style.flexContentContainer}>
-                      {/* Content Area Goes here */}
-                      <h1>WELCOME TO PREPARE PACIFIC</h1>
-                      <h4>THESE ARE SUBHEADINGS</h4>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse posuere, nisi vitae porta vehicula, enim ipsum accumsan enim, sed molestie diam sem eget nisi. Etiam tincidunt orci ullamcorper, venenatis odio nec, tincidunt odio. Maecenas porta leo sed lorem ullamcorper, sit amet consectetur nibh lobortis. Morbi id est quis elit tincidunt luctus. Curabitur dignissim ante a eleifend fringilla. Pellentesque varius malesuada tellus at vehicula. Sed in urna nulla. Morbi vel tincidunt augue. Cras congue ac lacus vitae semper. Nam metus arcu, tincidunt iaculis faucibus cursus, euismod nec velit.
-                          <br /><br />
-                          Donec dapibus dapibus odio ac varius. Proin dignissim tortor quis sagittis aliquet. Ut lacinia odio nec pharetra finibus. Curabitur id lacus vitae nunc mattis iaculis vitae vel erat. Sed et blandit leo, in sollicitudin metus. Cras lobortis turpis augue, sed pulvinar enim interdum vel. Maecenas vel risus eget purus pulvinar rutrum. Sed dolor dui, euismod a vulputate et, rhoncus id massa. Etiam eget lorem sed diam consequat ultrices. Suspendisse sit amet porta diam, id auctor metus. Nullam viverra ut est et consequat. Nunc cursus fermentum gravida.
-                      </p>
+                  {/* Grid Section Column 1 */}
+                  <div className={style.gridSections}>
+                    <div className={style.flexContainer}>
+                      <div className={style.pacificPattern}>
+                      {/* <PatternBrown /> */}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className={style.gridColumn}>
-                <div className={style.flexContainer}>
-                    <div className={style.flexContentContainer}>
-                      {/* Content Area Goes here */}
-                     
-                    </div>
+                  
+                  {/* Grid Section Column 2 */}
+                  <div className={style.gridSections}>
+                      
+                      {/* Grid Section Container */}
+                      <div className={style.gridTwoColumnContainer}>
+                        <div className={style.gridColumn}>
+                          <div className={style.flexContainer}>
+                            <div className={style.flexContentContainer}>
+                              {/* Content Area Goes here */}
+                              <h1>WELCOME TO PREPARE PACIFIC</h1>
+                              <h4>THESE ARE SUBHEADINGS</h4>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse posuere, nisi vitae porta vehicula, enim ipsum accumsan enim, sed molestie diam sem eget nisi. Etiam tincidunt orci ullamcorper, venenatis odio nec, tincidunt odio. Maecenas porta leo sed lorem ullamcorper, sit amet consectetur nibh lobortis. Morbi id est quis elit tincidunt luctus. Curabitur dignissim ante a eleifend fringilla. Pellentesque varius malesuada tellus at vehicula. Sed in urna nulla. Morbi vel tincidunt augue. Cras congue ac lacus vitae semper. Nam metus arcu, tincidunt iaculis faucibus cursus, euismod nec velit.
+                                  <br /><br />
+                                  Donec dapibus dapibus odio ac varius. Proin dignissim tortor quis sagittis aliquet. Ut lacinia odio nec pharetra finibus. Curabitur id lacus vitae nunc mattis iaculis vitae vel erat. Sed et blandit leo, in sollicitudin metus. Cras lobortis turpis augue, sed pulvinar enim interdum vel. Maecenas vel risus eget purus pulvinar rutrum. Sed dolor dui, euismod a vulputate et, rhoncus id massa. Etiam eget lorem sed diam consequat ultrices. Suspendisse sit amet porta diam, id auctor metus. Nullam viverra ut est et consequat. Nunc cursus fermentum gravida.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={style.gridColumn}>
+                        <div className={style.flexContainer}>
+                            <div className={style.flexContentContainer}>
+                              {/* Content Area Goes here */}
+                            
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                   </div>
-                </div>
-              </div>
-              
-          </div>
 
           {/* Grid Section Column 3 */}
           <div className={style.gridSections}></div>
         </div>
-      </section>
-
-
-      {/* ///////////////////////// */}
-      {/* Section Two (.sectionTwo) */}
-      <section id="sectionTwo" className={`${style.sectionWrapper} ${style.sectionTwo}`}>
+      </div>
+    </div>
+    {/* Section Two (.sectionTwo)///////////// */}
+    <div className="section">
+       
+      <div id="sectionTwo" className={`${style.sectionWrapper} ${style.sectionTwo}`}>
         {/* Grid Container */}
         <div className={style.gridContainer}>
           
@@ -140,11 +148,10 @@ const Home = (props) => {
           {/* Grid Section Column 3 */}
           <div className={style.gridSections}></div>
         </div>
-      </section>
-
-
-      {/* ///////////////////////// */}
-      {/* Section Three (.sectionThree) */}
+      </div>
+    </div>
+     {/* Section Three (.sectionThree) ///////////*/}
+    <div className="section">
       <section id="sectionThree" className={`${style.sectionWrapper} ${style.sectionThree}`}>
         {/* Grid Container */}
         <div className={style.gridContainer}>
@@ -209,11 +216,10 @@ const Home = (props) => {
           <div className={style.gridSections}></div>
         </div>
       </section>
-
-
-      {/* ///////////////////////// */}
-      {/* Section Four (.sectionFour) */}
-      <section id="sectionFour" className={`${style.sectionWrapper} ${style.sectionFour}`}>
+    </div>
+      {/* Section Four (.sectionFour) /////////////*/}
+    <div className="section">
+        <div id="sectionFour" className={`${style.sectionWrapper} ${style.sectionFour}`}>
          {/* Grid Container */}
          <div className={style.gridContainer}>
           
@@ -262,12 +268,11 @@ const Home = (props) => {
           {/* Grid Section Column 3 */}
           <div className={style.gridSections}></div>
         </div>
-      </section>
-
-
-      {/* ///////////////////////// */}
-      {/* Section Five (.sectionFive) */}
-      <section id="sectionFive" className={`${style.sectionWrapper} ${style.sectionFive}`}>
+      </div>
+    </div>
+    <div className="section">
+ {/* Section Five (.sectionFive) */}
+ <div id="sectionFive" className={`${style.sectionWrapper} ${style.sectionFive}`}>
          {/* Grid Container */}
          <div className={style.gridContainer}>
           
@@ -330,15 +335,16 @@ const Home = (props) => {
           {/* Grid Section Column 3 */}
           <div className={style.gridSections}></div>
         </div>
-      </section>
+      </div>
     </div>
-    
     {/* FOOTER SECTION
     **Footer is called here***/}
     {/* <Footer/> */}
    
- 
-    </>
+    </ReactFullpage.Wrapper>
+    }
+  />
+</>
 
   )
 
